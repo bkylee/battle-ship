3256a22d474430458f5e122e428c0a99242639d4
bkylee <brian.ky.lee@outlook.com>
2022-09-25 20:07:07 -0400

    did most of the webpack stuff except add jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = { 
+    mode: 'development',
     entry: './src/index.js',
     output:{
-        filename: '[bundle].js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Battle Ship',
+        }),
+    ],
+    module: {
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ['style-laoder', 'css-loader'],
+            },
+            {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+            },
+            {
+                test:/\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 };
